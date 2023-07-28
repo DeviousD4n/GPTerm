f9f0c04a2fb6337ff62036d75229209eee4ca3e6
DeviousD4n <129655658+DeviousD4n@users.noreply.github.com>
2023-07-29 03:25:00 +1000

    Update gpterm.py

--- gpterm.py
+++ gpterm.py
@@ -4,10 +4,14 @@ import sys
 import datetime
 import json
 import openai
+import subprocess
 
-openai.api_key = "YOUR_API_KEY"
+openai.api_key = "API_KEY"
+block_types = ["arduino","bash","c","cpp","csharp","css","diff","go","graphql","java","javascript","json","kotlin","latex","less","lua","makefile","makefile","markdown","matlab","mathematica","nginx","objectivec","perl","pgsql","php-template","php","plaintext","python-repl","python","r","ruby","rust","scss","shell","sql","swift","typescript","vbnet","wasm","xml","yaml"]
 messages = [{"role": "system", "content": "You are a command line tool running on " + str(dict(line.strip().split('=') for line in open('/etc/os-release', 'r')).get('PRETTY_NAME', '')) + ". Succinct replies are better than long-winded explanations."}]
 model = "gpt-3.5-turbo" #"gpt-3.5-turbo-16k"
+code_blocks = {} 
+block_id = 1
 
 BOLD = "\033[1m"
 ITALIC = "\033[3m"
@@ -15,26 +19,44 @@ RESET = "\033[0m"
 GPTCOLOR = "\033[38;5;99m"
 BLOCKCOLOR = "\033[38;5;200m"
 USERCOLOR = "\033[38;5;75m"
+COPYCOLOR = "\033[38;5;30m"
+ERROR = "\033[38;5;1m"
 
 
 def chat_stream(content):
+    global block_id
     messages.append({"role": "user", "content": content})
     try:
         response = openai.ChatCompletion.create(model=model, messages=messages, stream=True)
         print(f"{RESET + GPTCOLOR}GPT: ", end='')
-        code_block = False
+        code_block, language_found  = False, False
         buffer, content = '', ''
+        code = ''
         for chunk in response:
             message = chunk['choices'][0]['delta'].get('content', '')
             content += message
-            for check in message:
-                buffer += check
-                if buffer.endswith('```'):
-                    code_block = not code_block
-                    buffer = ''
-                elif len(buffer) > 3:
-                    print(f"{ITALIC + BLOCKCOLOR if code_block else RESET + GPTCOLOR}{buffer[0]}", end='', flush=True)
-                    buffer = buffer[1:]
+            if not language_found and code_block and message.strip() in block_types:
+                print(f"{BOLD + BLOCKCOLOR}{message}{RESET}", end='', flush=True) # code block language
+                language_found = not language_found
+                    
+            else: # chunk stream to check for code blocks
+                for check in message: 
+                    buffer += check
+                    if code_block:
+                        code += check
+                    if buffer.endswith('```'):
+                        code_block = not code_block
+                        buffer = ''
+                        if not code_block:
+                            code_blocks[str(block_id)] = code[:-3]
+                            print(f"\n{RESET + COPYCOLOR}╚═ codeblock {block_id} ═╝")
+                            block_id += 1
+                            code = '' 
+                            language_found = False
+
+                    elif len(buffer) > 3:
+                        print(f"{ITALIC + BLOCKCOLOR if code_block else RESET + GPTCOLOR}{buffer[0]}", end='', flush=True)
+                        buffer = buffer[1:]
 
         print(f"{BLOCKCOLOR if code_block else GPTCOLOR}{buffer}")
         return content 
@@ -63,32 +85,47 @@ def save_chat(chat_history, chat_exists=None):
 
 
 def chat_loop(resume_chat=None):
-    while True:
-        request = input(f"\n{BOLD + USERCOLOR}ASK: ")
-        if request in ('!quit', '!exit', '!q'):
-            save_chat(messages, resume_chat)
-            break
+    try:
+        while True:
+            request = input(f"\n{BOLD + USERCOLOR}ASK: ")
+            if request in ('!quit', '!q'):
+                save_chat(messages, resume_chat)
+                break
+            
+            elif request == "!kill":
+                break
 
-        elif request == "!multi":
-            print("Multi-line input. Enter '!end' on a newline to finish:")  
-            contents = []
-            while True:
-                line = input()
-                if line == "!end":
-                    break
-                contents.append(line)
-            request = '\n'.join(contents)
-        
-        elif request == "!history":  
-            for msg in messages:
-                role = 'GPT' if msg['role'] == 'assistant' else msg['role']
-                color = GPTCOLOR if role == 'GPT' else USERCOLOR
-                print(f"{color}{role.upper()}: {msg['content']}\n")
-            continue
+            elif request.startswith('!copy '):
+                copy_text = code_blocks.get(request.split(' ')[1], None)
+                if copy_text:
+                    subprocess.run('xclip -selection clipboard', input=copy_text, universal_newlines=True, check=True, shell=True)
+                    print(f"{RESET + COPYCOLOR}Copied to clipboard")
+                else:
+                    print(f"{RESET + ERROR}Invalid input")
+                continue
 
-        content = chat_stream(request)
-        messages.append({"role": "assistant", "content": content})
+            elif request == "!multi":
+                print("Multi-line input. Enter '!end' on a newline to finish:")  
+                contents = []
+                while True:
+                    line = input()
+                    if line == "!end":
+                        break
+                    contents.append(line)
+                request = '\n'.join(contents)
+            
+            elif request == "!history":  
+                for msg in messages:
+                    role = 'GPT' if msg['role'] == 'assistant' else msg['role']
+                    color = GPTCOLOR if role == 'GPT' else USERCOLOR
+                    print(f"{color}{role.upper()}: {msg['content']}\n")
+                continue
 
+            content = chat_stream(request)
+            messages.append({"role": "assistant", "content": content})
+
+    except KeyboardInterrupt:
+        save_chat(messages, resume_chat)
 
 if len(sys.argv) > 1:
     if sys.argv[1] == '-l':
@@ -108,11 +145,35 @@ if len(sys.argv) > 1:
             chat_loop(resume_chat)
 
     elif sys.argv[1] == '-h': 
-        print("""GPTerm for ChatGPT in the terminal. Version 0.1
-  Use without flags for a standard chat: 'gpt', chats are saved on 'quit' or 'exit'. Entering 'multi' allows for pasting of multiple line data such as code. Finish multi-line entry with 'END' on a newline.
-  Use with question in quotes for single reply: 'gpt "Convert 100 meters to feet"'.
-    -l  List all previous chats (stored in ~/.local/share/gpterm)
-    -r CHATNAME Resume a previous chat""")
+        print("""
+    GPTerm: A Command-Line Interface for ChatGPT. Version 0.1
+    
+    USAGE:
+        gpt [OPTION]... [QUESTION]
+
+    DESCRIPTION:
+        GPTerm interacts with ChatGPT in a terminal-based environment.
+
+    OPTIONS:
+        -l                  Lists all previous chats stored in ~/.local/share/gpterm.
+        -r CHAT_NAME        Resumes a previous chat session. CHAT_NAME should be replaced with the name of the chat file.
+        -h                  Displays this help message and exits.
+              
+    CHAT COMMANDS:
+        !quit or !q         Ends the current chat and saves it.
+        !kill               Ends the current chat without saving.
+        !copy CODEBLOCK_ID  Copies the specified code block to the clipboard. Replace CODEBLOCK_ID with the ID of the code block.
+        !history            Prints the current or resumed chat session history.
+        !multi              Initiates multi-line input mode. Useful for pasting data with multiple lines such as code. 
+                            Finish multi-line entry with '!end' on a newline.
+
+    EXAMPLES:
+        gpt
+        gpt "Convert 100 meters to feet"
+        gpt -l
+        gpt -r example_chat.json
+        gpt -h
+    """)
     else:
         chat_stream(sys.argv[1])
 
